bd8fa954a97119201ff5fe1169d2928ccc7c4c03
Javi <javiermespino@gmail.com>
2023-03-20 13:36:47 +0000

    Test(refactor): should_return_error_message_if_column_is_negative

--- src/core/wordWrap.ts
+++ src/core/wordWrap.ts
@@ -1,6 +1,10 @@
 export const wrap = (text: string, column: number) => {
+    checkIfNegativeColumn(column);
+    return text;
+}
+
+const checkIfNegativeColumn = (column: number) => {
     if (column < 0) {
         throw new Error(`Column can't be negative and it is ${column}`);
     }
-    return text;
 }
