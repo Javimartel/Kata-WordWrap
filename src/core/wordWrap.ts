84d3f7506d6902412d4998e8f9ef89f1e6bdc00d
Javi <javiermespino@gmail.com>
2023-03-20 13:49:24 +0000

    Test(green): should_return_the_string_with_column_number_as_line_breaks

--- src/core/wordWrap.ts
+++ src/core/wordWrap.ts
@@ -1,6 +1,6 @@
 export const wrap = (text: string, column: number) => {
     checkIfNegativeColumn(column);
-    return text;
+    return text.replace(text[column], `\n${text[column]}`);
 }
 
 const checkIfNegativeColumn = (column: number) => {
