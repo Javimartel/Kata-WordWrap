8ded0f464a653f8733f3f8970306efa23dd4321a
Javi <javiermespino@gmail.com>
2023-03-20 14:01:41 +0000

    Test(red): should_return_the_text_with_multiple_line_breaks

--- src/test/wordWrap.test.ts
+++ src/test/wordWrap.test.ts
@@ -1,7 +1,7 @@
 import { wrap } from "../core/wordWrap";
 
 describe('Word Wrap test', () => {
-    it('should return the same string if it is empty', () => {
+    it('should return the same text if it is empty', () => {
         expect(wrap("", 1)).toBe("");
     })
     
@@ -9,7 +9,11 @@ describe('Word Wrap test', () => {
         expect(() => wrap("hello", -2)).toThrow("Column can't be negative and it is -2");
     });
 
-    it('should return the string with column number as line breaks', () => {
+    it('should return the text with column number as line breaks', () => {
         expect(wrap("hello", 2)).toBe("he\nllo");
+    })
+
+    it('should return the text with multiple line breaks', () => {
+        expect(wrap("hello world", 2)).toBe("he\nll\no \nwo\nrl\nd");
     })
 })
